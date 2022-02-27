8bc07194aa9b43aa04d60cbe311b08d08a24cfed
Daniel Melo <daniel.melo42@outlook.com>
2022-02-26 20:07:44 -0700

    cors and heroku file added

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const userRouter = require('./routes/userRouter');
 const taskRouter = require('./routes/taskRouter')
 const meetingRouter = require('./routes/meetingRouter')
 
 const app = express();
 
+app.use(cors());
+app.options('*', cors());
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tasks', taskRouter);
