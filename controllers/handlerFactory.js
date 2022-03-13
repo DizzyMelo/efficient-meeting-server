abbd14b2cda25351c90a870492f98149a1e17d30
Daniel Melo <daniel.melo42@outlook.com>
2022-03-12 22:30:14 -0700

    response data simplified

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -65,9 +65,7 @@ exports.getOne = (Model, popOptions) =>
     }
     res.status(200).json({
       status: 'success',
-      data: {
-        data: doc,
-      },
+      data: doc,
     });
   });
 
