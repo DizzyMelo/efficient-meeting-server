df340cb618bbe32e6c0da5a151f16c0234a0ab3b
Daniel Melo <daniel.melo42@outlook.com>
2022-04-01 22:41:12 -0600

    list only tasks assigned to user

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,6 +1,7 @@
 const Task = require('../models/taskModel')
 const factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
 
 exports.createTask = async (req, res, next) => {
     req.body.createdBy = req.user.id;
@@ -11,7 +12,16 @@ exports.createTask = async (req, res, next) => {
     });
 }
 
-exports.getTasks = factory.getAll(Task)
+exports.getTasks = catchAsync(async (req, res, next) => {
+    tasks = await Task.find({assignedTo: req.user.id})
+
+    res.status(200).json({
+        status: 'success',
+        requestedAt: req.requestTime,
+        results: tasks.length,
+        tasks
+    });
+})
 exports.getTask = factory.getOne(Task)
 exports.updateTask = factory.updateOne(Task)
 exports.deleteTask = factory.deleteOne(Task)
