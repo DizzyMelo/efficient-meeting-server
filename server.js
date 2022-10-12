55ef6751a08482b973964d7e01430bd3a97a3f3b
Daniel Melo <daniel.melo42@outlook.com>
2022-10-12 09:12:47 -0600

    new database connection string and removed sensitive data from repo

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 
-dotenv.config({ path: './config.env' });
+dotenv.config();
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
 mongoose
-  .connect("mongodb+srv://daniel-melo:Dado201094@cluster0.wtuon.mongodb.net/efficient-meeting?retryWrites=true&w=majority", {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     // useCreateIndex: true,
     // useFindAndModify: false,
